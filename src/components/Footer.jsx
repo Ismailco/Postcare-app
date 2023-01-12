a9e5380fd1d5037e0dc3b7415dcb83503a93a1e5
Ismail Courr <contact@ismailcourr.ml>
2023-01-12 14:20:50 +0100

    Finish NavBar & Footer first design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,29 +2,31 @@ import React from 'react';
 import { footerLinksOne, footerLinksTwo, footerLinksThree, copyRightText } from '../constants';
 
 const Footer = () => (
-  <footer className="w-full p-4 absolute bottom-0">
-    <div className="flex justify-center items-center">
-      <img className="w-full" src="../assets/logo.png" alt="logo" />
-      <ul className="flex flex-col mr-20">
+  <footer className="w-full p-4 absolute bottom-0 bg-[#bbb] text-[#828282] font-medium">
+    <div className="flex justify-center items-start">
+      <div className="w-full">
+        <img className="w-36 h-20 bg-gray-400" src="../assets/logo.png" alt="logo" />
+      </div>
+      <ul className="flex flex-col mr-20 w-56">
         {footerLinksOne.map((link, index) => (
-          <li key={index}>
+          <li className="hover:underline" key={index}>
             <a href={link.path}>{link.name}</a>
           </li>
         ))}
       </ul>
-      <ul className="flex flex-col mr-20">
-        {footerLinksOne.map((link, index) => (
-          <li key={index}>
+      <ul className="flex flex-col mr-20 w-56">
+        {footerLinksTwo.map((link, index) => (
+          <li className="hover:underline" key={index}>
             <a href={link.path}>{link.name}</a>
           </li>
         ))}
       </ul>
     </div>
-    <hr className="my-4" />
+    <hr className="my-4 bg-white" />
     <div className="flex justify-between items-center">
       <ul className="flex">
-        {footerLinksOne.map((link, index) => (
-          <li className="mr-2" key={index}>
+        {footerLinksThree.map((link, index) => (
+          <li className="mr-2 hover:underline" key={index}>
             <a href={link.path}>{link.name}</a>
           </li>
         ))}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,6 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
-    colors: {
-      transparent: 'transparent',
-      mainWhite: 'E5E7EB',
-    },
   },
   plugins: [],
 };
