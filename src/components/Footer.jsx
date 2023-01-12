ae6fd5a2b1a019d0af5fabb311b884c2968cdeb9
Ismail Courr <contact@ismailcourr.ml>
2023-01-12 13:53:12 +0100

    Finish the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { footerLinksOne, footerLinksTwo, footerLinksThree, copyRightText } from '../constants';
 
-const Footer = () => {
-  return (
-    <section>
-      <h1>Footer</h1>
-    </section>
-  );
-};
+const Footer = () => (
+  <footer className="w-full p-4 absolute bottom-0">
+    <div className="flex justify-center items-center">
+      <img className="w-full" src="../assets/logo.png" alt="logo" />
+      <ul className="flex flex-col mr-20">
+        {footerLinksOne.map((link, index) => (
+          <li key={index}>
+            <a href={link.path}>{link.name}</a>
+          </li>
+        ))}
+      </ul>
+      <ul className="flex flex-col mr-20">
+        {footerLinksOne.map((link, index) => (
+          <li key={index}>
+            <a href={link.path}>{link.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <hr className="my-4" />
+    <div className="flex justify-between items-center">
+      <ul className="flex">
+        {footerLinksOne.map((link, index) => (
+          <li className="mr-2" key={index}>
+            <a href={link.path}>{link.name}</a>
+          </li>
+        ))}
+      </ul>
+      <p className="text-center">{copyRightText}</p>
+    </div>
+  </footer>
+);
 
 export default Footer;
