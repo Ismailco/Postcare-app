6d444ccd688c63f54a58e59f3266f3da3d8d25a8
Ismail Courr <contact@ismailcourr.ml>
2023-01-14 16:07:21 +0100

    Create signup page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 const Login = () => (
   <div className="flex justify-center items-center w-full py-20">
-    <section className="flex flex-col border border-[#111827] w-4/5 m-4 p-4">
+    <section className="rounded flex flex-col border border-[#111827] w-4/5 m-4 p-4">
       <div className="flex flex-col justify-center items-center bg-[#F9FAFB]">
         <h1 className="font-bold text-2xl my-2">Login to your account</h1>
         <p>Enter your email and password to login</p>
       </div>
       <form className="flex flex-col my-4">
-        <label htmlFor="email">Email address</label>
+        <label className="text-[#374151]" htmlFor="email">
+          Email address
+        </label>
         <input className="border border-[#111827] p-2 mb-4" type="email" id="email" placeholder="example@gmail.com" />
-        <label htmlFor="password">Password</label>
+        <label className="text-[#374151]" htmlFor="password">
+          Password
+        </label>
         <input className="border border-[#111827] p-2 mb-4" type="password" id="password" placeholder="**************" />
-        <button className="bg-[#bbb] p-2 my-4" type="submit">
+        <button className="bg-[#bbb] p-2 my-4 text-white" type="submit">
           Login
         </button>
       </form>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import profile_pic from '../assets/img/profile.png';
 
-const Signup = () => {
-  return <div>Signup</div>;
-};
+const Signup = () => (
+  <div className="flex justify-center items-center w-full py-20">
+    <section className="rounded flex flex-col border border-[#111827] w-4/5 m-4 p-4">
+      <div className="flex flex-col justify-center items-center bg-[#F9FAFB]">
+        <h1 className="font-bold text-2xl my-2">Sign up</h1>
+        <p>Create an account</p>
+      </div>
+      <div className="flex justify-center items-center">
+        <img className="my-6" src={profile_pic} alt="profile" />
+      </div>
+      <form className="flex flex-col my-4">
+        <label className="text-[#374151]" htmlFor="name">
+          Name
+        </label>
+        <input className="border border-[#111827] p-2 mb-4" type="name" id="name" placeholder="John Smith" />
+        <label className="text-[#374151]" htmlFor="email">
+          Email address
+        </label>
+        <input className="border border-[#111827] p-2 mb-4" type="email" id="email" placeholder="example@gmail.com" />
+        <label className="text-[#374151]" htmlFor="password">
+          Password
+        </label>
+        <input className="border border-[#111827] p-2 mb-4" type="password" id="password" placeholder="**************" />
+        <label className="text-[#374151]" htmlFor="password">
+          Confirm Password
+        </label>
+        <input className="border border-[#111827] p-2 mb-4" type="password" id="password" placeholder="**************" />
+        <button className="bg-[#bbb] p-2 my-4 text-white" type="submit">
+          Create Account
+        </button>
+      </form>
+
+      <p className="text-[#374151]">
+        By signing up, you agree to our{' '}
+        <a href="#" className="underline">
+          Terms of Conditions
+        </a>{' '}
+        and{' '}
+        <a href="#" className="underline">
+          Privacy Policy
+        </a>
+      </p>
+    </section>
+  </div>
+);
 
 export default Signup;
