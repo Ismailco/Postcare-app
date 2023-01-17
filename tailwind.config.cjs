f6193e0bdee801e15ed6eece3ed732a870fe0aa9
Ismail Courr <contact@ismailcourr.ml>
2023-01-17 18:49:23 +0100

    Add colors and pressets

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,19 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      white: '#fff',
+      black: '#000',
+      light: '#e9edf0',
+      dark: '#bbb',
+      primary: '#333399',
+      secondary: '#46b2e0',
+      accent: {
+        100: 'fa3e6f',
+        200: 'febc6a',
+      },
+    },
   },
   plugins: [],
 };
