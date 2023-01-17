1a4b8b6cba41a3677b6db8441e131b29fb2606d6
Ismail Courr <contact@ismailcourr.ml>
2023-01-17 20:05:02 +0100

    Fix linters

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,6 @@ import Signup from './Signup';
 import Landing from './Landing';
 import NotFound404 from './NotFound404';
 
-export { Login, Signup, Landing, NotFound404 };
+export {
+  Login, Signup, Landing, NotFound404,
+};
