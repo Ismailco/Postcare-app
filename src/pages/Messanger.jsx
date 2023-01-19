912ffd33f00d222a3bc3954e9ac0e24aa53a1c50
Ismail Courr <contact@ismailcourr.ml>
2023-01-19 16:15:59 +0100

    Fix pages overflow

--- src/pages/Messanger.jsx
+++ src/pages/Messanger.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { profile } from '../assets/img/index';
 
 const Messanger = () => (
-  <main className="flex flex-col m-4">
+  <main className="flex flex-col m-4 w-full">
     <section className="m-4">
       <h1 className="text-2xl font-bold mb-4">Messenger</h1>
       <p>Your secure, online, healthcare tracking site. Helping you manage your personal health information with your practices, anytime and anywhere.</p>
@@ -12,7 +13,10 @@ const Messanger = () => (
       <article className="flex flex-col md:flex-row justify-between items-center border p-4">
         <img className="rounded-full mr-4" src={profile} alt="profile" />
         <div className="flex flex-col justify-center items-center md:items-start text-center md:text-start md:w-3/4 md:mr-4">
-          <h2 className="text-xl font-bold">Provider Name</h2>
+          {/* The link redirect to the Chat of the specefic provider (change it later) */}
+          <Link to="/messanger/1">
+            <h2 className="text-xl font-bold hover:underline">Provider Name</h2>
+          </Link>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
         </div>
         <div className="flex md:flex-col self-end md:self-start mt-4">
@@ -23,7 +27,10 @@ const Messanger = () => (
       <article className="flex flex-col md:flex-row justify-between items-center border p-4">
         <img className="rounded-full mr-4" src={profile} alt="profile" />
         <div className="flex flex-col justify-center items-center md:items-start text-center md:text-start md:w-3/4 md:mr-4">
-          <h2 className="text-xl font-bold">Provider Name</h2>
+          {/* The link redirect to the Chat of the specefic provider (change it later) */}
+          <Link to="/messanger/1">
+            <h2 className="text-xl font-bold hover:underline">Provider Name</h2>
+          </Link>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
         </div>
         <div className="flex md:flex-col self-end md:self-start mt-4">
@@ -34,7 +41,10 @@ const Messanger = () => (
       <article className="flex flex-col md:flex-row justify-between items-center border p-4">
         <img className="rounded-full mr-4" src={profile} alt="profile" />
         <div className="flex flex-col justify-center items-center md:items-start text-center md:text-start md:w-3/4 md:mr-4">
-          <h2 className="text-xl font-bold">Provider Name</h2>
+          {/* The link redirect to the Chat of the specefic provider (change it later) */}
+          <Link to="/messanger/1">
+            <h2 className="text-xl font-bold hover:underline">Provider Name</h2>
+          </Link>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
         </div>
         <div className="flex md:flex-col self-end md:self-start mt-4">
