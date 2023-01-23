c8776a29b5ba80a86d2a2e018afd20b1fdeb81b5
Ismail Courr <contact@ismailcourr.ml>
2023-01-23 02:59:57 +0100

    Create the PWA - SEO best practices

--- src/pages/ToDoList.jsx
+++ src/pages/ToDoList.jsx
@@ -23,12 +23,12 @@ const ToDoList = () => {
   }, []);
 
   return (
-    <main className="flex flex-col m-4 w-full">
-      <section className="my-6">
+    <main className="flex flex-col p-4 w-full">
+      <section className="my-6 xl:ml-6">
         <h1 className="text-2xl font-bold mb-4">To Do List</h1>
         <p>Your secure, online, healthcare tracking site. Helping you manage your personal health information with your practices, anytime and anywhere.</p>
       </section>
-      <section className="my-6">
+      <section className="my-6 xl:ml-6">
         <article className="flex justify-end items-center">
           <button id="monthly-btn" className={`${activeBtn === 'monthly-btn' ? 'text-black/80 bg-light' : 'bg-dark text-white hover:bg-light hover:text-black/80'} font-bold py-2 px-4 rounded-full mr-4`} onClick={(e) => handleClick(e)}>
             Monthly
