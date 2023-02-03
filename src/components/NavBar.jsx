ccc055224a72bb8a504e6235d9052b45fe979cb1
Ismail Courr <contact@ismailcourr.ml>
2023-02-03 19:36:01 +0100

    style navigation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,20 +13,20 @@ const NavBar = () => {
   return (
     <nav className="w-full h-[80px] flex px-2 md:px-6 justify-between items-center bg-primary font-medium">
       <Link to="/">
-        <img className="hidden md:block w-36 h-10" src={whiteLogo} alt="logo" />
+        <img className="hidden md:block w-36 h-10 ml-5" src={whiteLogo} alt="logo" />
         <img className="md:hidden w-20 h-10" src={logo} alt="logo" />
       </Link>
       <ul className="NavLinks flex">
         {!logedIn &&
           navLinks.map((link, index) => (
-            <li className={`${index === navLinks.length - 1 ? 'mr-2 bg-secondary hover:bg-accent-100' : 'mr-4'} text-white rounded-full p-2 duration-500`} key={index}>
+            <li className={`${index === navLinks.length - 1 ? 'mr-2 px-4 bg-secondary hover:bg-accent-100' : 'mr-4'} text-white font-bold rounded-full p-2 duration-500`} key={index}>
               <Link to={link.path}>{link.name}</Link>
             </li>
           ))}
       </ul>
       {logedIn && (
         <button onClick={handdleLogout} className={`mr-2 hover:bg-secondary bg-accent-100 mr-4 text-white rounded-full p-2 duration-500`}>
-          Signout
+          SIGN OUT
         </button>
       )}
     </nav>
