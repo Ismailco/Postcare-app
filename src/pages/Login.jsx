27106de00f74df2d2939ccfa8d43dc93a33b8a40
Ismail Courr <contact@ismailcourr.ml>
2023-02-03 19:29:12 +0100

    Change Login & signup styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,28 +40,30 @@ const Login = () => {
   }, []);
 
   return (
-    <div className="flex justify-center items-center w-full py-28">
-      <section className="rounded flex flex-col border border-[#111827] lg:w-1/3 w-4/5 m-4 p-4">
-        <div className="flex flex-col justify-center items-center bg-[#F9FAFB]">
-          <h1 className="font-bold text-2xl my-2">Login to your account</h1>
+    <div className="flex justify-center items-center w-full py-10">
+      <section className="rounded-md flex flex-col border border-2 border-primary md:w-1/2 xl:w-1/3 w-96 m-4 p-4">
+        <div className="flex flex-col justify-center items-center my-4">
+          <h1 className="font-bold text-2xl mb-1">Login to your account</h1>
           <p>Enter your email and password to login</p>
         </div>
-        <form className="flex flex-col my-4 on" onSubmit={(e) => handleSubmit(e)}>
+        <form className="flex flex-col my-2 on" onSubmit={(e) => handleSubmit(e)}>
           <label className="text-[#374151]" htmlFor="email">
             Email address
           </label>
-          <input required className="border border-[#111827] p-2 mb-4" type="email" id="email" placeholder="example@gmail.com" />
+          <input required className="border border-[#111827] rounded-md p-2 mb-4" type="email" id="email" placeholder="example@gmail.com" />
           <label className="text-[#374151]" htmlFor="password">
             Password
           </label>
-          <input required className="border border-[#111827] p-2 mb-4" type="password" id="password" placeholder="**************" minLength="8" />
-          <button className="bg-[#bbb] p-2 my-4 text-white" type="submit">
-            Login
+          <input required className="border border-[#111827] rounded-md p-2 mb-4" type="password" id="password" placeholder="**************" minLength="8" />
+          <button className="bg-primary rounded-full p-2 mt-4 font-bold text-white" type="submit">
+            LOGIN
           </button>
         </form>
-
-        <p className="text-center">
-          <span className="text-[#6B7280]">Don't have an account?</span> <Link to="/signup">Sign up</Link>
+        <p className="text-center my-4">
+          <span className="text-[#6B7280]">Don't have an account?</span>{' '}
+          <Link to="/signup" className="text-[#46b2e0]">
+            Sign up
+          </Link>
         </p>
       </section>
     </div>
